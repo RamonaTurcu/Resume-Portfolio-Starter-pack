97f349f966441500312b9a6a2f14519ee6d95dc7
Ramona Turcu <41471228+RamonaTurcu@users.noreply.github.com>
2021-02-03 18:28:25 +0200

    warning fixes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Quote from "./Components/Quote";
-import Portfolio from "./Components/Portfolio";
 
 import "./App.css";
 
